9a44aad97cd1a76c836360ab527b7bcb5fe93484
Mariam Reda <mariamreda2911@gmail.com>
2024-05-27 15:22:22 +0200

    Update app.py

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ def get_conversational_chain():
     Answer:
     """
 
-    model = ChatOpenAI()
+    model = ChatOpenAI(model_name="gpt-3.5-turbo-16k")
 
     prompt = PromptTemplate(
         template=prompt_template, input_variables=["context", "question"]
